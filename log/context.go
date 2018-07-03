fabric8-services/fabric8-wit

Rename project to Fabric8-WIT (#1450)
Related to #345

log/context.go
@@ -3,7 +3,7 @@ package log
 import (
 	"context"
 
-	tokencontext "github.com/almighty/almighty-core/login/tokencontext"
+	tokencontext "github.com/fabric8-services/fabric8-wit/login/tokencontext"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/goadesign/goa/client"

log/log.go
@@ -6,13 +6,13 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/almighty/almighty-core/configuration"
+	"github.com/fabric8-services/fabric8-wit/configuration"
 
 	"context"
 	log "github.com/Sirupsen/logrus"
 )
 
-const defaultPackageName = "github.com/almighty/almighty-core/"
+const defaultPackageName = "github.com/fabric8-services/fabric8-wit/"
 
 var (
 	logger = &log.Logger{

fabric8-services/fabric8-common

log/context.go
@@ -3,7 +3,7 @@ package log
 import (
 	"context"
 
-	tokencontext "github.com/almighty/almighty-core/login/tokencontext"
+	tokencontext "github.com/fabric8-services/fabric8-common/login/tokencontext"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/goadesign/goa/client"

log/log.go
@@ -6,13 +6,13 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/almighty/almighty-core/configuration"
+	"github.com/fabric8-services/fabric8-common/configuration"
 
 	"context"
 	log "github.com/Sirupsen/logrus"
 )
 
-const defaultPackageName = "github.com/almighty/almighty-core/"
+const defaultPackageName = "github.com/fabric8-services/fabric8-common/"
 
 var (
 	logger = &log.Logger{