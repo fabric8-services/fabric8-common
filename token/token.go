fabric8-services/fabric8-wit

Rename project to Fabric8-WIT (#1450)
Related to #345

token/token.go
@@ -4,8 +4,8 @@ import (
 	"crypto/rsa"
 
 	"context"
-	"github.com/almighty/almighty-core/account"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fabric8-services/fabric8-wit/account"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"

token/token_test.go
@@ -6,11 +6,11 @@ import (
 
 	"context"
 
-	"github.com/almighty/almighty-core/account"
-	"github.com/almighty/almighty-core/resource"
-	testtoken "github.com/almighty/almighty-core/test/token"
-	"github.com/almighty/almighty-core/token"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fabric8-services/fabric8-wit/account"
+	"github.com/fabric8-services/fabric8-wit/resource"
+	testtoken "github.com/fabric8-services/fabric8-wit/test/token"
+	"github.com/fabric8-services/fabric8-wit/token"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"

fabric8-services/fabric8-common

token/token.go
@@ -4,8 +4,8 @@ import (
 	"crypto/rsa"
 
 	"context"
-	"github.com/almighty/almighty-core/account"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fabric8-services/fabric8-common/account"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"

token/token_test.go
@@ -6,11 +6,11 @@ import (
 
 	"context"
 
-	"github.com/almighty/almighty-core/account"
-	"github.com/almighty/almighty-core/resource"
-	testtoken "github.com/almighty/almighty-core/test/token"
-	"github.com/almighty/almighty-core/token"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fabric8-services/fabric8-common/account"
+	"github.com/fabric8-services/fabric8-common/resource"
+	testtoken "github.com/fabric8-services/fabric8-common/test/token"
+	"github.com/fabric8-services/fabric8-common/token"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"