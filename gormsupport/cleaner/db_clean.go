fabric8-services/fabric8-wit

Rename project to Fabric8-WIT (#1450)
Related to #345

gormsupport/cleaner/db_clean.go
@@ -3,8 +3,8 @@ package cleaner
 import (
 	"database/sql"
 
-	"github.com/almighty/almighty-core/log"
-	"github.com/almighty/almighty-core/workitem"
+	"github.com/fabric8-services/fabric8-wit/log"
+	"github.com/fabric8-services/fabric8-wit/workitem"
 	uuid "github.com/satori/go.uuid"
 
 	"fmt"

gormsupport/lifecycle.go
@@ -3,7 +3,7 @@ package gormsupport
 import (
 	"time"
 
-	"github.com/almighty/almighty-core/convert"
+	"github.com/fabric8-services/fabric8-wit/convert"
 	"github.com/jinzhu/gorm"
 )
 

gormsupport/lifecycle_blackbox_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/almighty/almighty-core/convert"
-	"github.com/almighty/almighty-core/gormsupport"
-	"github.com/almighty/almighty-core/resource"
+	"github.com/fabric8-services/fabric8-wit/convert"
+	"github.com/fabric8-services/fabric8-wit/gormsupport"
+	"github.com/fabric8-services/fabric8-wit/resource"
 	"github.com/stretchr/testify/assert"
 )
 

fabric8-services/fabric8-common

gormsupport/cleaner/db_clean.go
@@ -3,8 +3,8 @@ package cleaner
 import (
 	"database/sql"
 
-	"github.com/almighty/almighty-core/log"
-	"github.com/almighty/almighty-core/workitem"
+	"github.com/fabric8-services/fabric8-common/log"
+	"github.com/fabric8-services/fabric8-common/workitem"
 	uuid "github.com/satori/go.uuid"
 
 	"fmt"

gormsupport/lifecycle.go
@@ -3,7 +3,7 @@ package gormsupport
 import (
 	"time"
 
-	"github.com/almighty/almighty-core/convert"
+	"github.com/fabric8-services/fabric8-common/convert"
 	"github.com/jinzhu/gorm"
 )
 

gormsupport/lifecycle_blackbox_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/almighty/almighty-core/convert"
-	"github.com/almighty/almighty-core/gormsupport"
-	"github.com/almighty/almighty-core/resource"
+	"github.com/fabric8-services/fabric8-common/convert"
+	"github.com/fabric8-services/fabric8-common/gormsupport"
+	"github.com/fabric8-services/fabric8-common/resource"
 	"github.com/stretchr/testify/assert"
 )
 