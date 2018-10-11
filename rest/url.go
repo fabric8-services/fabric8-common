alexeykazakov/fabric8-auth

renamed packages

rest/url.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"strings"
 
-	"github.com/fabric8-services/fabric8-wit/errors"
+	"github.com/fabric8-services/fabric8-auth/errors"
 
 	"github.com/goadesign/goa"
 )

rest/url_blackbox_test.go
@@ -5,7 +5,7 @@ import (
 
 	"net/http"
 
-	"github.com/fabric8-services/fabric8-wit/resource"
+	"github.com/fabric8-services/fabric8-auth/resource"
 	"github.com/goadesign/goa"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"

fabric8-services/fabric8-common

rest/url.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"strings"
 
-	"github.com/fabric8-services/fabric8-wit/errors"
+	"github.com/fabric8-services/fabric8-common/errors"
 
 	"github.com/goadesign/goa"
 )

rest/url_blackbox_test.go
@@ -5,7 +5,7 @@ import (
 
 	"net/http"
 
-	"github.com/fabric8-services/fabric8-wit/resource"
+	"github.com/fabric8-services/fabric8-common/resource"
 	"github.com/goadesign/goa"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"