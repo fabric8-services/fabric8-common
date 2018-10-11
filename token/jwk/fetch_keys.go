fabric8-services/fabric8-cluster

Extracting cluster service from Auth. Initial commit

token/jwk/fetch_keys.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/rest"
+	"github.com/fabric8-services/fabric8-cluster/log"
+	"github.com/fabric8-services/fabric8-cluster/rest"
 
 	"gopkg.in/square/go-jose.v2"
 )

token/jwk/fetch_keys_blackbox_test.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/fabric8-services/fabric8-auth/test"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	"github.com/fabric8-services/fabric8-auth/test/token"
-	"github.com/fabric8-services/fabric8-auth/token/jwk"
+	"github.com/fabric8-services/fabric8-cluster/test"
+	testsuite "github.com/fabric8-services/fabric8-cluster/test/suite"
+	"github.com/fabric8-services/fabric8-cluster/test/token"
+	"github.com/fabric8-services/fabric8-cluster/token/jwk"
 
 	"bytes"
 	"io/ioutil"

token/signer/signer.go
@@ -2,8 +2,8 @@ package signer
 
 import (
 	"context"
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/token"
+	"github.com/fabric8-services/fabric8-cluster/log"
+	"github.com/fabric8-services/fabric8-cluster/token"
 	goaclient "github.com/goadesign/goa/client"
 )
 

token/signer/signer_whitebox_test.go
@@ -1,9 +1,9 @@
 package signer
 
 import (
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	tokentestsupport "github.com/fabric8-services/fabric8-auth/test/token"
-	tokenutil "github.com/fabric8-services/fabric8-auth/token"
+	testsuite "github.com/fabric8-services/fabric8-cluster/test/suite"
+	tokentestsupport "github.com/fabric8-services/fabric8-cluster/test/token"
+	tokenutil "github.com/fabric8-services/fabric8-cluster/token"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"net/http"

token/token.go
@@ -14,15 +14,15 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fabric8-services/fabric8-auth/account"
-	"github.com/fabric8-services/fabric8-auth/account/repository"
-	authclient "github.com/fabric8-services/fabric8-auth/client"
-	autherrors "github.com/fabric8-services/fabric8-auth/errors"
-	"github.com/fabric8-services/fabric8-auth/goasupport"
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/rest"
-	"github.com/fabric8-services/fabric8-auth/token/jwk"
-	"github.com/fabric8-services/fabric8-auth/token/tokencontext"
+	"github.com/fabric8-services/fabric8-cluster/account"
+	"github.com/fabric8-services/fabric8-cluster/account/repository"
+	authclient "github.com/fabric8-services/fabric8-cluster/client"
+	autherrors "github.com/fabric8-services/fabric8-cluster/errors"
+	"github.com/fabric8-services/fabric8-cluster/goasupport"
+	"github.com/fabric8-services/fabric8-cluster/log"
+	"github.com/fabric8-services/fabric8-cluster/rest"
+	"github.com/fabric8-services/fabric8-cluster/token/jwk"
+	"github.com/fabric8-services/fabric8-cluster/token/tokencontext"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/goadesign/goa"
@@ -39,7 +39,7 @@ const (
 
 	// Service Account Names
 
-	Auth               = "fabric8-auth"
+	Auth               = "fabric8-cluster"
 	WIT                = "fabric8-wit"
 	OsoProxy           = "fabric8-oso-proxy"
 	Tenant             = "fabric8-tenant"

token/token_test.go
@@ -8,15 +8,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/fabric8-services/fabric8-auth/account"
-	"github.com/fabric8-services/fabric8-auth/account/repository"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	testtoken "github.com/fabric8-services/fabric8-auth/test/token"
-	"github.com/fabric8-services/fabric8-auth/token"
-	"github.com/fabric8-services/fabric8-auth/token/tokencontext"
+	"github.com/fabric8-services/fabric8-cluster/account"
+	"github.com/fabric8-services/fabric8-cluster/account/repository"
+	testsuite "github.com/fabric8-services/fabric8-cluster/test/suite"
+	testtoken "github.com/fabric8-services/fabric8-cluster/test/token"
+	"github.com/fabric8-services/fabric8-cluster/token"
+	"github.com/fabric8-services/fabric8-cluster/token/tokencontext"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/fabric8-services/fabric8-auth/errors"
+	"github.com/fabric8-services/fabric8-cluster/errors"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	"github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"

token/token_whitebox_test.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"testing"
 
-	config "github.com/fabric8-services/fabric8-auth/configuration"
-	"github.com/fabric8-services/fabric8-auth/resource"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
+	config "github.com/fabric8-services/fabric8-cluster/configuration"
+	"github.com/fabric8-services/fabric8-cluster/resource"
+	testsuite "github.com/fabric8-services/fabric8-cluster/test/suite"
 
 	"github.com/dgrijalva/jwt-go"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
@@ -63,7 +63,7 @@ func (s *TestWhiteboxTokenSuite) tokenManagerWithAuthURL() (*tokenManager, strin
 func (s *TestWhiteboxTokenSuite) TestAuthServiceAccountGeneratedOK() {
 	m, authURL := s.tokenManagerWithAuthURL()
 	tokenString := m.AuthServiceAccountToken()
-	s.checkServiceAccountToken(tokenString, AuthServiceAccountID, "fabric8-auth", authURL)
+	s.checkServiceAccountToken(tokenString, AuthServiceAccountID, "fabric8-cluster", authURL)
 
 }
 
@@ -212,7 +212,7 @@ func (s *TestWhiteboxTokenSuite) TestAuthServiceAccount() {
 
 	claims := token.Claims.(jwt.MapClaims)
 	require.Equal(s.T(), AuthServiceAccountID, claims["sub"])
-	require.Equal(s.T(), "fabric8-auth", claims["service_accountname"])
+	require.Equal(s.T(), "fabric8-cluster", claims["service_accountname"])
 	require.Equal(s.T(), []interface{}{"uma_protection"}, claims["scopes"])
 	jti, ok := claims["jti"].(string)
 	require.True(s.T(), ok)

fabric8-services/fabric8-common

token/jwk/fetch_keys.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/rest"
+	"github.com/fabric8-services/fabric8-common/log"
+	"github.com/fabric8-services/fabric8-common/rest"
 
 	"gopkg.in/square/go-jose.v2"
 )

token/jwk/fetch_keys_blackbox_test.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/fabric8-services/fabric8-auth/test"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	"github.com/fabric8-services/fabric8-auth/test/token"
-	"github.com/fabric8-services/fabric8-auth/token/jwk"
+	"github.com/fabric8-services/fabric8-common/test"
+	testsuite "github.com/fabric8-services/fabric8-common/test/suite"
+	"github.com/fabric8-services/fabric8-common/test/token"
+	"github.com/fabric8-services/fabric8-common/token/jwk"
 
 	"bytes"
 	"io/ioutil"

token/signer/signer.go
@@ -2,8 +2,8 @@ package signer
 
 import (
 	"context"
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/token"
+	"github.com/fabric8-services/fabric8-common/log"
+	"github.com/fabric8-services/fabric8-common/token"
 	goaclient "github.com/goadesign/goa/client"
 )
 

token/signer/signer_whitebox_test.go
@@ -1,9 +1,9 @@
 package signer
 
 import (
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	tokentestsupport "github.com/fabric8-services/fabric8-auth/test/token"
-	tokenutil "github.com/fabric8-services/fabric8-auth/token"
+	testsuite "github.com/fabric8-services/fabric8-common/test/suite"
+	tokentestsupport "github.com/fabric8-services/fabric8-common/test/token"
+	tokenutil "github.com/fabric8-services/fabric8-common/token"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"net/http"

token/token.go
@@ -14,15 +14,15 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fabric8-services/fabric8-auth/account"
-	"github.com/fabric8-services/fabric8-auth/account/repository"
-	authclient "github.com/fabric8-services/fabric8-auth/client"
-	autherrors "github.com/fabric8-services/fabric8-auth/errors"
-	"github.com/fabric8-services/fabric8-auth/goasupport"
-	"github.com/fabric8-services/fabric8-auth/log"
-	"github.com/fabric8-services/fabric8-auth/rest"
-	"github.com/fabric8-services/fabric8-auth/token/jwk"
-	"github.com/fabric8-services/fabric8-auth/token/tokencontext"
+	"github.com/fabric8-services/fabric8-common/account"
+	"github.com/fabric8-services/fabric8-common/account/repository"
+	authclient "github.com/fabric8-services/fabric8-common/client"
+	autherrors "github.com/fabric8-services/fabric8-common/errors"
+	"github.com/fabric8-services/fabric8-common/goasupport"
+	"github.com/fabric8-services/fabric8-common/log"
+	"github.com/fabric8-services/fabric8-common/rest"
+	"github.com/fabric8-services/fabric8-common/token/jwk"
+	"github.com/fabric8-services/fabric8-common/token/tokencontext"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/goadesign/goa"
@@ -39,7 +39,7 @@ const (
 
 	// Service Account Names
 
-	Auth               = "fabric8-auth"
+	Auth               = "fabric8-cluster"
 	WIT                = "fabric8-wit"
 	OsoProxy           = "fabric8-oso-proxy"
 	Tenant             = "fabric8-tenant"

token/token_test.go
@@ -8,15 +8,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/fabric8-services/fabric8-auth/account"
-	"github.com/fabric8-services/fabric8-auth/account/repository"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
-	testtoken "github.com/fabric8-services/fabric8-auth/test/token"
-	"github.com/fabric8-services/fabric8-auth/token"
-	"github.com/fabric8-services/fabric8-auth/token/tokencontext"
+	"github.com/fabric8-services/fabric8-common/account"
+	"github.com/fabric8-services/fabric8-common/account/repository"
+	testsuite "github.com/fabric8-services/fabric8-common/test/suite"
+	testtoken "github.com/fabric8-services/fabric8-common/test/token"
+	"github.com/fabric8-services/fabric8-common/token"
+	"github.com/fabric8-services/fabric8-common/token/tokencontext"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/fabric8-services/fabric8-auth/errors"
+	"github.com/fabric8-services/fabric8-common/errors"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
 	"github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"

token/token_whitebox_test.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"testing"
 
-	config "github.com/fabric8-services/fabric8-auth/configuration"
-	"github.com/fabric8-services/fabric8-auth/resource"
-	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
+	config "github.com/fabric8-services/fabric8-common/configuration"
+	"github.com/fabric8-services/fabric8-common/resource"
+	testsuite "github.com/fabric8-services/fabric8-common/test/suite"
 
 	"github.com/dgrijalva/jwt-go"
 	goajwt "github.com/goadesign/goa/middleware/security/jwt"
@@ -63,7 +63,7 @@ func (s *TestWhiteboxTokenSuite) tokenManagerWithAuthURL() (*tokenManager, strin
 func (s *TestWhiteboxTokenSuite) TestAuthServiceAccountGeneratedOK() {
 	m, authURL := s.tokenManagerWithAuthURL()
 	tokenString := m.AuthServiceAccountToken()
-	s.checkServiceAccountToken(tokenString, AuthServiceAccountID, "fabric8-auth", authURL)
+	s.checkServiceAccountToken(tokenString, AuthServiceAccountID, "fabric8-cluster", authURL)
 
 }
 
@@ -212,7 +212,7 @@ func (s *TestWhiteboxTokenSuite) TestAuthServiceAccount() {
 
 	claims := token.Claims.(jwt.MapClaims)
 	require.Equal(s.T(), AuthServiceAccountID, claims["sub"])
-	require.Equal(s.T(), "fabric8-auth", claims["service_accountname"])
+	require.Equal(s.T(), "fabric8-cluster", claims["service_accountname"])
 	require.Equal(s.T(), []interface{}{"uma_protection"}, claims["scopes"])
 	jti, ok := claims["jti"].(string)
 	require.True(s.T(), ok)