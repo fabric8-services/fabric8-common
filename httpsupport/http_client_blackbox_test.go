aeb26b3b05a79ddc49a5356df87642d1af8217f0
Xavier Coulon <xcoulon@redhat.com>
2018-10-15 16:08:37 +0200

    refactor HTTP query params tests
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- httpsupport/http_client_blackbox_test.go
+++ httpsupport/http_client_blackbox_test.go
@@ -1,6 +1,7 @@
 package httpsupport_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"testing"
@@ -41,54 +42,57 @@ func TestAddParams(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
 	t.Parallel()
 
-	t.Run("success", func(t *testing.T) {
+	t.Run("with one plus one query params", func(t *testing.T) {
+		// when
+		generatedURLString, err := httpsupport.AddParam("https://openshift.io", "param1", "a")
+		// then
+		require.NoError(t, err)
+		assert.Equal(t, "https://openshift.io?param1=a", generatedURLString)
+		// when adding an extra query param
+		generatedURLString, err = httpsupport.AddParam(generatedURLString, "param2", "abc")
+		// then
+		require.NoError(t, err)
+		equalURLs(t, &url.URL{
+			Scheme:"https",
+			Host:"openshift.io",
+			Path: "",
+			RawQuery:"param1=a&param2=abc",
+		}, generatedURLString)
+	})
+
+	t.Run("with three query params at once", func(t *testing.T) {
 		// given
-		testMap := map[string]string{
+		scheme := "https"
+		host := "openshift.io"
+		path := "/api/status"
+		queryParams := map[string]string{
 			"param1": "a",
 			"param2": "b",
 			"param3": "https://www.redhat.com",
 		}
-		testHost := "openshift.io"
 		// when
-		generatedURLString, err := httpsupport.AddParams("https://"+testHost, testMap)
-		// then
+		generatedURLString, err := httpsupport.AddParams(fmt.Sprintf("%s://%s%s", scheme, host, path), queryParams)
+		// then verify that the generated URL can be parsed that all params were set as expected
 		require.NoError(t, err)
-		// verify that the generated URL can be parsed that all params were set as expected
-		generateURL, err := url.Parse(generatedURLString)
-		require.NoError(t, err)
-		assert.Equal(t, testHost, generateURL.Host)
-		assert.Equal(t, "https", generateURL.Scheme)
-		m, err := url.ParseQuery(generateURL.RawQuery)
-		require.NoError(t, err)
-		for k, v := range testMap {
-			assert.Equal(t, v, m[k][0])
-		}
+		equalURLs(t, &url.URL{
+			Scheme:"https",
+			Host:host,
+			Path: path,
+			RawQuery:"param1=a&param2=b&param3=https%3A%2F%2Fwww.redhat.com",
+		}, generatedURLString)
 	})
-}
 
-func TestAddParamSuccess(t *testing.T) {
-	resource.Require(t, resource.UnitTest)
-	t.Parallel()
 
-	t.Run("success", func(t *testing.T) {
-		generatedURL, err := httpsupport.AddParam("https://openshift.io", "param1", "a")
-		require.NoError(t, err)
-		assert.Equal(t, "https://openshift.io?param1=a", generatedURL)
-
-		generatedURL, err = httpsupport.AddParam(generatedURL, "param2", "abc")
-		require.NoError(t, err)
-		equalURLs(t, "https://openshift.io?param1=a&param2=abc", generatedURL)
-	})
 }
 
 // Can't use test.EqualURLs() because of cycle dependency
-func equalURLs(t *testing.T, expected string, actual string) {
-	expectedURL, err := url.Parse(expected)
-	require.Nil(t, err)
+func equalURLs(t *testing.T, expectedURL *url.URL, actual string) {
+	require.Equal(t, expectedURL.String(), actual)
 	actualURL, err := url.Parse(actual)
 	require.Nil(t, err)
 	assert.Equal(t, expectedURL.Scheme, actualURL.Scheme)
 	assert.Equal(t, expectedURL.Host, actualURL.Host)
+	assert.Equal(t, expectedURL.Path, actualURL.Path)
 	assert.Equal(t, len(expectedURL.Query()), len(actualURL.Query()))
 	for name, value := range expectedURL.Query() {
 		assert.Equal(t, value, actualURL.Query()[name])
