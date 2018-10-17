fabric8-services/fabric8-auth

Add WWW-Authenticate: LOGIN header to the error response when login is required (#519)
- [x] `WWW-Authenticate: LOGIN ur="https://auth.openshift.io/api/login" description="re-login is required"` header is added if any request to Auth contains an invalid bearer token: `Authorization: bearer ${token}`
- [x] The same `WWW-Authenticate: LOGIN` header added to the 401 response if https://auth.openshift.io/api/token/refresh endpoint got 401 from Keycloak

This PR is required to fix the backend side of https://github.com/openshiftio/openshift.io/issues/3746
Also part of https://github.com/openshiftio/openshift.io/issues/3736

goamiddleware/jwt_token_context_whitebox_test.go
@@ -1,0 +1,77 @@
+package goamiddleware
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/textproto"
+	"testing"
+
+	testsuite "github.com/fabric8-services/fabric8-auth/test/suite"
+	testtoken "github.com/fabric8-services/fabric8-auth/test/token"
+
+	"github.com/goadesign/goa"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestJWTokenContext(t *testing.T) {
+	suite.Run(t, &TestJWTokenContextSuite{})
+}
+
+type TestJWTokenContextSuite struct {
+	testsuite.UnitTestSuite
+}
+
+func (s *TestJWTokenContextSuite) TestHandler() {
+	schema := &goa.JWTSecurity{}
+	errUnauthorized := goa.NewErrorClass("token_validation_failed", 401)
+
+	rw := httptest.NewRecorder()
+	rq := &http.Request{Header: make(map[string][]string)}
+	h := handler(testtoken.TokenManager, schema, dummyHandler, errUnauthorized)
+
+	err := h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "whoops, security scheme with location (in) \"\" not supported", err.Error())
+
+	// OK if no Authorization header
+	schema.In = "header"
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+
+	// OK if not bearer
+	schema.Name = "Authorization"
+	rq.Header.Set("Authorization", "something")
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+
+	// Get 401 if token is invalid
+	rq.Header.Set("Authorization", "bearer token")
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Contains(s.T(), err.Error(), "401 token_validation_failed: token is invalid", err.Error())
+	assert.Equal(s.T(), "LOGIN url=http://localhost/api/login, description=\"re-login is required\"", rw.Header().Get("WWW-Authenticate"))
+	assert.Contains(s.T(), rw.Header().Get("Access-Control-Expose-Headers"), "WWW-Authenticate")
+
+	// OK if token is valid
+	rw = httptest.NewRecorder()
+	t, err := testtoken.TokenManager.GenerateServiceAccountToken(uuid.NewV4().String(), "sa-name")
+	require.NoError(s.T(), err)
+	rq.Header.Set("Authorization", "bearer "+t)
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+	header := textproto.MIMEHeader(rw.Header())
+	assert.NotContains(s.T(), header, "WWW-Authenticate")
+	assert.NotContains(s.T(), header, "Access-Control-Expose-Headers")
+}
+
+func dummyHandler(ctx context.Context, rw http.ResponseWriter, r *http.Request) error {
+	return errors.New("next-handler-error")
+}

fabric8-services/fabric8-common

goamiddleware/jwt_token_context_whitebox_test.go
@@ -1,0 +1,77 @@
+package goamiddleware
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/textproto"
+	"testing"
+
+	testsuite "github.com/fabric8-services/fabric8-common/test/suite"
+	testtoken "github.com/fabric8-services/fabric8-common/test/token"
+
+	"github.com/goadesign/goa"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestJWTokenContext(t *testing.T) {
+	suite.Run(t, &TestJWTokenContextSuite{})
+}
+
+type TestJWTokenContextSuite struct {
+	testsuite.UnitTestSuite
+}
+
+func (s *TestJWTokenContextSuite) TestHandler() {
+	schema := &goa.JWTSecurity{}
+	errUnauthorized := goa.NewErrorClass("token_validation_failed", 401)
+
+	rw := httptest.NewRecorder()
+	rq := &http.Request{Header: make(map[string][]string)}
+	h := handler(testtoken.TokenManager, schema, dummyHandler, errUnauthorized)
+
+	err := h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "whoops, security scheme with location (in) \"\" not supported", err.Error())
+
+	// OK if no Authorization header
+	schema.In = "header"
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+
+	// OK if not bearer
+	schema.Name = "Authorization"
+	rq.Header.Set("Authorization", "something")
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+
+	// Get 401 if token is invalid
+	rq.Header.Set("Authorization", "bearer token")
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Contains(s.T(), err.Error(), "401 token_validation_failed: token is invalid", err.Error())
+	assert.Equal(s.T(), "LOGIN url=http://localhost/api/login, description=\"re-login is required\"", rw.Header().Get("WWW-Authenticate"))
+	assert.Contains(s.T(), rw.Header().Get("Access-Control-Expose-Headers"), "WWW-Authenticate")
+
+	// OK if token is valid
+	rw = httptest.NewRecorder()
+	t, err := testtoken.TokenManager.GenerateServiceAccountToken(uuid.NewV4().String(), "sa-name")
+	require.NoError(s.T(), err)
+	rq.Header.Set("Authorization", "bearer "+t)
+	err = h(context.Background(), rw, rq)
+	require.Error(s.T(), err)
+	assert.Equal(s.T(), "next-handler-error", err.Error())
+	header := textproto.MIMEHeader(rw.Header())
+	assert.NotContains(s.T(), header, "WWW-Authenticate")
+	assert.NotContains(s.T(), header, "Access-Control-Expose-Headers")
+}
+
+func dummyHandler(ctx context.Context, rw http.ResponseWriter, r *http.Request) error {
+	return errors.New("next-handler-error")
+}