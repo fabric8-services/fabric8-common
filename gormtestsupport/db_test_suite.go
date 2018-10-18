alexeykazakov/fabric8-auth

renamed packages

gormtestsupport/db_test_suite.go
@@ -3,12 +3,12 @@ package gormtestsupport
 import (
 	"os"
 
-	config "github.com/fabric8-services/fabric8-wit/configuration"
-	"github.com/fabric8-services/fabric8-wit/log"
-	"github.com/fabric8-services/fabric8-wit/migration"
-	"github.com/fabric8-services/fabric8-wit/models"
-	"github.com/fabric8-services/fabric8-wit/resource"
-	"github.com/fabric8-services/fabric8-wit/workitem"
+	config "github.com/fabric8-services/fabric8-auth/configuration"
+	"github.com/fabric8-services/fabric8-auth/log"
+	"github.com/fabric8-services/fabric8-auth/migration"
+	"github.com/fabric8-services/fabric8-auth/models"
+	"github.com/fabric8-services/fabric8-auth/resource"
+	"github.com/fabric8-services/fabric8-auth/workitem"
 
 	"context"
 

fabric8-services/fabric8-common

gormtestsupport/db_test_suite.go
@@ -3,12 +3,12 @@ package gormtestsupport
 import (
 	"os"
 
-	config "github.com/fabric8-services/fabric8-wit/configuration"
-	"github.com/fabric8-services/fabric8-wit/log"
-	"github.com/fabric8-services/fabric8-wit/migration"
-	"github.com/fabric8-services/fabric8-wit/models"
-	"github.com/fabric8-services/fabric8-wit/resource"
-	"github.com/fabric8-services/fabric8-wit/workitem"
+	config "github.com/fabric8-services/fabric8-common/configuration"
+	"github.com/fabric8-services/fabric8-common/log"
+	"github.com/fabric8-services/fabric8-common/migration"
+	"github.com/fabric8-services/fabric8-common/models"
+	"github.com/fabric8-services/fabric8-common/resource"
+	"github.com/fabric8-services/fabric8-common/workitem"
 
 	"context"
 