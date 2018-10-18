fabric8-services/fabric8-auth

Implement create team and list team endpoints and service (#471)
This PR implements the following endpoints:

### Create a new team:

```
POST /teams

{ 
  "space-id":"aceb30ec-aec2-44db-98cf-d4a884d5a7df",
  "name":"Auth Team"
}
```

_Response:_

```
201 Created

{
  "team-id":"6cddfc92-7703-49aa-8e35-b93f5850de6d"
}
```

### List teams for the current user:

```
GET /teams
```

_Response:_

```
200 OK

{
  "data": [{
    "id": "6cddfc92-7703-49aa-8e35-b93f5850de6d",
    "name":"Auth Team",
    "space-id":"aceb30ec-aec2-44db-98cf-d4a884d5a7df",
    "space-name":"openshift-io",
    "member":"true",
    "roles":[]
  }]
}
```

### List teams for a space:

```
GET /spaces/aceb30ec-aec2-44db-98cf-d4a884d5a7df/teams
```

_Response:_

```
200 OK

{
  "data":[{
    "id": "6cddfc92-7703-49aa-8e35-b93f5850de6d",
    "name":"Auth Team"
  }]
}
```

Fixes #469

gormtestsupport/db_test_suite.go
@@ -11,6 +11,7 @@ import (
 	"github.com/fabric8-services/fabric8-auth/log"
 	"github.com/fabric8-services/fabric8-auth/migration"
 	"github.com/fabric8-services/fabric8-auth/resource"
+	"github.com/fabric8-services/fabric8-auth/test/graph"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq" // need to import postgres driver
@@ -101,4 +102,8 @@ func (s *DBTestSuite) DisableGormCallbacks() func() {
 		s.DB.Callback().Create().Register(gormCallbackName, oldCreateCallback)
 		s.DB.Callback().Update().Register(gormCallbackName, oldUpdateCallback)
 	}
+}
+
+func (s *DBTestSuite) NewTestGraph() graph.TestGraph {
+	return graph.NewTestGraph(s.T(), s.Application, s.Ctx, s.DB)
 }

fabric8-services/fabric8-common

gormtestsupport/db_test_suite.go
@@ -11,6 +11,7 @@ import (
 	"github.com/fabric8-services/fabric8-common/log"
 	"github.com/fabric8-services/fabric8-common/migration"
 	"github.com/fabric8-services/fabric8-common/resource"
+	"github.com/fabric8-services/fabric8-common/test/graph"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq" // need to import postgres driver
@@ -101,4 +102,8 @@ func (s *DBTestSuite) DisableGormCallbacks() func() {
 		s.DB.Callback().Create().Register(gormCallbackName, oldCreateCallback)
 		s.DB.Callback().Update().Register(gormCallbackName, oldUpdateCallback)
 	}
+}
+
+func (s *DBTestSuite) NewTestGraph() graph.TestGraph {
+	return graph.NewTestGraph(s.T(), s.Application, s.Ctx, s.DB)
 }